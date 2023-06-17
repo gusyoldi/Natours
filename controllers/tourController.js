3c9a89d232f10673168b4a51fdccaac2baa72248
Elroli09 <yoldi.gustavo@gmail.com>
2023-06-17 19:26:09 -0300

    Refactoriza tourController con protocolos HTTP hacia la nueva DB.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,90 +1,91 @@
-const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
 
-const checkId = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
+    res.status(200).json({
+      status: 'succes',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
-      message: 'invalid ID',
+      message: err,
     });
   }
-
-  next();
 };
 
-const checkBody = (req, res, next) => {
-  console.log('Soy checkBody 👮‍♂️');
-
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+const getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // const tour = await Tour.findOne({_id: req.params.id})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: err,
     });
   }
-
-  next();
 };
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'succes',
-    results: tours.length,
-    requestTime: req.requestTime,
-    data: {
-      tours,
-    },
-  });
+const createNewTour = async (req, res) => {
+  // const newTour = new Tour({});
+  // newTour.save();
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(200).json({
+      status: 'success',
+      data: newTour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(201).json({
-    status: 'succes',
-    data: {
-      tour,
-    },
-  });
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'succes',
+      data: tour,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-const createNewTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) return console.log(err);
-
-      res.status(201).json({
-        status: 'succes',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'succes',
-    data: '<Changed data...>',
-  });
-};
-
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'succes',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'succes',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 module.exports = {
@@ -93,6 +94,4 @@ module.exports = {
   createNewTour,
   updateTour,
   deleteTour,
-  checkId,
-  checkBody,
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const {
-  checkId,
-  checkBody,
   getAllTours,
   createNewTour,
   getTour,
   updateTour,
   deleteTour,
 } = require('../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id', checkId);
+// router.param('id', checkId);
 
-router.route('/').get(getAllTours).post(checkBody, createNewTour);
+router.route('/').get(getAllTours).post(createNewTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
